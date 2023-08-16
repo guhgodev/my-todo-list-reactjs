16584e030ab420f221de7beedbe0bfeb26b57820
Gustavo Oliveira <gustavogodev@gmail.com>
2023-08-16 15:41:00 -0300

    Finalized basic structure of App.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,11 @@ function App() {
           <div className="todo">
             <div className="content">
               <p>{todo.text}</p>
+              <p className="category">({todo.category}) </p>
+            </div>
+            <div>
+            <button>Finalizar</button>
+            <button>x</button>
             </div>
           </div>
         ))}
